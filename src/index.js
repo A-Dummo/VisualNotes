2bfa4cfe8b260e33fa1719c13565bb0161ba585b
Dan <2014adamw@gmail.com>
2025-11-06 05:19:17 -0800

    Added to index files.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Implement panning by translating container through css
+// If container no longer a container, then should it be an anchor?
+// Should there be an actual anchor element which notes're relative to?
+const container = document.querySelector("#container");
